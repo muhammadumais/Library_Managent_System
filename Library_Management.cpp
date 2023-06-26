9acb18f547b0ecad4a5d1b9196455f353134e13b
muhammadumais <muhammadumais.121@gmail.com>
2023-06-26 15:54:37 +0500

    my first project

---
+++ Library_Management.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class Book{
+  int id;
+  static int temp;
+  string name;
+  string author;
+
+  public: 
+    Book(){
+      id = temp + 1;
+      name = "";
+      author = "";
+      temp++;
+    }
+
+    Book(string namee, string authorr){
+      id = temp + 1;
+      name = namee;
+      author = authorr;
+      temp++;
+    }
+
+    int get_id(){
+      return id;
+    }
+
+    string get_name(){
+      return name;
+    }
+
+    string get_author(){
+      return author;
+    }
+
+    void set_name(string namee){
+       name = namee;
+    }
+
+    void set_author(string authorr){
+       author = authorr;
+    }
+
+};
+
+int Book :: temp = 0;
+
+void fill_book_data(Book* book){
+  string temp;
+  cout << "----------Enter Book Data-----------" << endl << endl;
+  cout << "Enter Name : ";
+  getline(cin, temp);
+  book->set_name(temp);
+  cout << "Enter Author : ";
+  getline(cin, temp);
+  book->set_author(temp);
+}
+
+class Library{
+
+  string name;
+  string location;
+  vector <Book*> books;
+
+  Library(){
+    name = "";
+    location = "";
+    books.push_back(new Book("Library_Intro", "N/A"));
+  }
+
+  void add_book(){
+    Book* temp;
+    fill_book_data(temp);
+    books.push_back(temp);
+  }
+
+};
